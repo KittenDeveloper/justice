0e468db852b3518d6e07f8f342466c50c5d6064c
KittenDeveloper <fuller.justice@gmail.com>
2017-05-10 11:28:19 -0700

    Array Manipulator

--- arrays.py
+++ arrays.py
@@ -1,11 +1,52 @@
-import csv
-import random
-array1=[]
-with open("example.csv", "r+") as csvf:
-	read = csv.DictReader(csvf)
-	for row in read:
-		array1.append((row['first'], row['last']))
-for i in array1:
-	itemsInarray=len(array1)-1
-	print array1[random.randint(0,itemsInarray)]
+import Tkinter
 
+global state
+global dict
+dict1={}
+state=1
+
+def appendfunc():
+	global state
+	state =2
+	search.pack_forget()
+	append.pack()
+def enterfunc():
+	global dict1
+	global state
+	if state == 1:
+		try:
+			lbl1.config(text=dict1[searchbox1.get()])
+		except:
+			lbl1.config(text="Invalid Search Entry")
+	elif state==2:
+		try:
+			dict1[entrybox1.get()]=entrybox2.get();
+		except:
+			lbl1.config(text="Invalid Append Entry")
+def searchfunc():
+	global state
+	state=1
+	search.pack()
+	append.pack_forget()
+	
+
+root=Tkinter.Tk()
+root.title("Array Manipulator v1.0")
+search=Tkinter.Frame(root)
+search.pack()
+appendbtn=Tkinter.Button(search, text="Append Mode", command=appendfunc)
+appendbtn.pack()
+append=Tkinter.Frame(root)
+searchbtn=Tkinter.Button(append, text="Search Mode", command=searchfunc)
+searchbtn.pack()
+searchbox1=Tkinter.Entry(search)
+searchbox1.pack()
+enterbtn=Tkinter.Button(root, text="Enter", command=enterfunc)
+enterbtn.pack()
+lbl1=Tkinter.Label(root)
+lbl1.pack()
+entrybox1=Tkinter.Entry(append)
+entrybox1.pack()
+entrybox2=Tkinter.Entry(append)
+entrybox2.pack()
+root.mainloop()
