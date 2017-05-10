957b8bf7fb1e2a6f3468719a2b91201fa7b63f3b
KittenDeveloper <fuller.justice@gmail.com>
2017-05-10 12:44:09 -0700

    gui fix

--- arrays.py
+++ arrays.py
@@ -8,6 +8,7 @@ state=1
 
 def appendfunc():
 	global state
+	searchbtn.pack(side='top')
 	state =2
 	search.pack_forget()
 	append.pack()
@@ -38,6 +39,7 @@ def enterfunc():
 def searchfunc():
 	global state
 	state=1
+	searchbtn.pack_forget()
 	search.pack()
 	append.pack_forget()
 	searchbox1.delete(0, 'end')
@@ -47,19 +49,18 @@ root=Tkinter.Tk()
 root.title("Array Manipulator v1.0")
 search=Tkinter.Frame(root)
 search.pack()
-appendbtn=Tkinter.Button(search, text="Append Mode", command=appendfunc)
-appendbtn.pack()
 append=Tkinter.Frame(root)
-searchbtn=Tkinter.Button(append, text="Search Mode", command=searchfunc)
-searchbtn.pack()
-searchbox1=Tkinter.Entry(search)
-searchbox1.pack()
 enterbtn=Tkinter.Button(root, text="Enter", command=enterfunc)
-enterbtn.pack()
+enterbtn.pack(side='bottom')
 lbl1=Tkinter.Label(root)
 lbl1.pack()
+searchbox1=Tkinter.Entry(search)
+searchbox1.grid(column=1, row =2)
+appendbtn=Tkinter.Button(search, text="Append Mode", command=appendfunc)
+appendbtn.grid(column=1,row =1)
+searchbtn=Tkinter.Button(root, text="Search Mode", command=searchfunc)
 entrybox1=Tkinter.Entry(append)
-entrybox1.pack()
+entrybox1.grid(column=1, row =2)
 entrybox2=Tkinter.Entry(append)
-entrybox2.pack()
+entrybox2.grid(column=2,row=2)
 root.mainloop()
