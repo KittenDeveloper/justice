fb46b895bd9e8a2450adcbd2e2d8d39285f257be
KittenDeveloper <fuller.justice@gmail.com>
2017-06-19 17:15:38 -0500

    Update client.rb

--- client.rb
+++ client.rb
@@ -1,9 +1,7 @@
 require 'socket'
 socket = TCPSocket.open('69.244.201.56',8091)
-puts "Enter name: "
-name = gets.chomp+": "
 loop do
 	puts socket.recv(1024)
-	socket.print(name+gets())
+	socket.print(gets())
 	Gem.win_platform? ? (system "cls") : (system "clear")
 end
