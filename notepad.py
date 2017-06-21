9504e963c08419081dc6762b8569f7565b23ea62
KittenDeveloper <fuller.justice@gmail.com>
2017-06-21 16:22:18 -0500

    Notebad

--- notepad.py
+++ notepad.py
@@ -4,21 +4,22 @@ def openfile():
 	global filenm;
 	file = tkFileDialog.askopenfile(parent=root,mode='r+',title='Choose a file')
 	if file != None:
+		text1.delete("1.0", Tkinter.END)
 		text1.insert('1.0', file.read())
 		filenm=file.name
 	file.close
 def savefile():
 	global filenm;
-	file=open(filenm, "r+")
-	thetext = text1.get('1.0', 'end')
+	file=open(filenm, "w")
+	thetext = text1.get('1.0', Tkinter.END)
 	file.write(thetext)
 	file.close
 root=Tkinter.Tk()
 root.title=("TEXTEDIT")
 text1= Tkinter.Text(root)
-text1.grid(row=1,column=1)
+text1.pack(fill=Tkinter.BOTH, expand=Tkinter.YES)
 btn = Tkinter.Button(root, text="Chose File", command=openfile)
-btn.grid(row=2,column=1)
+btn.pack()
 btn1 = Tkinter.Button(root, text="Save File", command=savefile)
-btn1.grid(row=3,column=1)
+btn1.pack()
 root.mainloop()
