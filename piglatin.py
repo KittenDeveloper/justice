4ad5db8b54df6de871a76cc011c8affcc7e5a0d7
KittenDeveloper <fuller.justice@gmail.com>
2017-06-28 17:20:06 -0500

    converter
    converts to pig latin

---
+++ piglatin.py
@@ -0,0 +1,11 @@
+import sys
+if (len(sys.argv)>2 or len(sys.argv)<2):
+	print("Usage: "+sys.argv[0]+" \"string to convert\"")
+	exit()
+def convertword(x):
+	firstchar=x[0]
+	modifiedword=x[1:]
+	return modifiedword+firstchar+"ay"
+def convertstring(x):
+	return list(map(convertword,x.split(" ")))
+print(convertstring(sys.argv[1]))
